80e5df0f930f7b3fa5b0c83f2111fff0ba09a1e5
h4nyu <yao.ntno@gmail.com>
2020-05-06 06:47:50 +0000

    config

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 NOISED_TGT_DIR = "/srv/store/noised_tgt"
 RAW_TGT_DIR = "/srv/store/raw"
 CROP_LEN = 74 // 2
-NOISE_FLOOR = 0.5
-NOISE_P = 0.3
-NOISE_HIGH = 0.3
+NOISE_FLOOR = 0.1
+NOISE_P = 0.4
+NOISE_HIGH = 0.4
 NOISE_LOW = 0.001
 MAX_POWER = 420
